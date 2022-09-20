a4413b583950d97a80e37754b7bedca175f485a6
gobeomjun <alap_u@naver.com>
2022-09-21 01:33:02 +0900

    feat - balls 도메인 생성

---
+++ src/main/java/baseball/domain/Balls.java
@@ -0,0 +1,21 @@
+package baseball.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Balls {
+
+    public final List<Ball> balls;
+
+    private Balls(List<Ball> balls) {
+        this.balls = balls;
+    }
+
+    public static Balls createBalls(List<String> numbers) {
+        List<Ball> balls =  numbers.stream().map(i -> (new Ball(Integer.parseInt(i)))).collect(Collectors.toList());
+        return new Balls(balls);
+    }
+
+
+
+}
