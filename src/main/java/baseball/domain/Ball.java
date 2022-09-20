f8e993e1591fb0d1848448199b4b22ba50b04da7
gobeomjun <alap_u@naver.com>
2022-09-21 01:39:38 +0900

    refactor - 매직넘버 치환

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -4,10 +4,20 @@ import java.util.Objects;
 
 public class Ball {
 
+    private static final String BALL_RANGE_MSG = "숫자의 범위는 1~9입니다.";
+    public static final int MIN_BALL_NUMBER = 1;
+    public static final int MAX_BALL_NUMBER = 9;
     private final int ball;
 
     public Ball(int ball) {
         this.ball = ball;
+    }
+
+    private int ballValidation(int ballNumber) {
+        if (ballNumber >= MIN_BALL_NUMBER || MAX_BALL_NUMBER <= ballNumber) {
+            return ballNumber;
+        }
+        throw new IllegalArgumentException(BALL_RANGE_MSG);
     }
 
     @Override
@@ -18,11 +28,12 @@ public class Ball {
         if (otherBall == null || getClass() != otherBall.getClass()) {
             return false;
         }
-        return ball == ((Ball)otherBall).ball;
+        return ball == ((Ball) otherBall).ball;
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(ball);
     }
+
 }
