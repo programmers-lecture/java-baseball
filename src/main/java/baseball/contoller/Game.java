29bcc8ca0eeb8e100db33f79f160d3cacde641b4
gobeomjun <alap_u@naver.com>
2022-09-21 21:24:32 +0900

    feat - 야구게임 시작 구성

---
+++ src/main/java/baseball/contoller/Game.java
@@ -0,0 +1,42 @@
+package baseball.contoller;
+
+import baseball.domain.Balls;
+import baseball.domain.Referee;
+import baseball.domain.Score;
+import baseball.util.RandomNumber;
+import baseball.util.RandomNumberImpl;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class Game {
+
+    public static final String REPALY_NUMBER = "1";
+    private InputView inputView = new InputView();
+    private RandomNumber randomNumber = new RandomNumberImpl();
+    private Referee referee;
+    private static boolean isReplay = true;
+
+    public void run() {
+        referee = new Referee(randomNumber.createRandomNumbers());
+        Score.initPoint();
+        while (!Score.isOutPoint()) {
+            Score.initPoint();
+            OutputView.printRequestNumberMSG();
+            referee.getScore(Balls.createBalls(inputView.inputNumbersToList()));
+            OutputView.printString(Score.pointToString());
+        }
+    }
+
+    public static boolean getIsReplay() {
+        return isReplay;
+    }
+
+    public void userIsReplay() {
+        OutputView.printAskReplay();
+        if (inputView.inputReplay().equals(REPALY_NUMBER)) {
+            this.isReplay = true;
+            return;
+        }
+        this.isReplay = false;
+    }
+}
