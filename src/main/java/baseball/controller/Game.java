16e57e8aa6c5c3112cc0eeb0b9679877343701b4
joona95 <joona95@naver.com>
2022-09-22 00:33:01 +0900

    feat: 전체 게임 실행 기능 추가

---
+++ src/main/java/baseball/controller/Game.java
@@ -0,0 +1,30 @@
+package baseball.controller;
+
+import baseball.models.Baseball;
+import baseball.models.Computer;
+import baseball.models.Player;
+import baseball.views.InputView;
+import baseball.views.OutputView;
+
+import java.util.List;
+
+public class Game {
+    private Game() {}
+
+    public static void runGame() {
+        boolean isGameActive = true;
+        while(isGameActive) {
+            Computer computer = new Computer();
+            Baseball baseball = new Baseball();
+            while(baseball.isBaseBallEnd()) {
+                String[] playerNumber = InputView.inputPlayerNumber();
+                Player player = new Player(playerNumber);
+                baseball.playBaseball(player.getBall(), computer.getBall());
+                OutputView.printBaseballResult(baseball.getBallCount(), baseball.getStrikeCount());
+            }
+            isGameActive = InputView.inputRerunOrNot();
+        }
+    }
+
+
+}
