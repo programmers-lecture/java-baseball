d46fa09f11006ed8145d2b8d058146a935cf5cbf
gobeomjun <alap_u@naver.com>
2022-09-21 21:16:11 +0900

    feat - 리플레이 여부

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,13 +3,18 @@ package baseball.view;
 public class OutputView {
 
     private static final String REQUEST_NUMBER_MSG = "숫자를 입력해주세요 : ";
+    public static final String ASK_REPLAY_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printRequestNumberMSG() {
         System.out.printf(REQUEST_NUMBER_MSG);
     }
 
-    public static void printString(String value){
+    public static void printString(String value) {
         System.out.println(value);
+    }
+
+    public static void printAskReplay() {
+        printString(ASK_REPLAY_MSG);
     }
 
 }
