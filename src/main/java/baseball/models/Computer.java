6d2d72ff4633e3f6060d55fcccfc9dca19b54064
joona95 <joona95@naver.com>
2022-09-22 00:47:36 +0900

    refactor: 추상 메소드 추가

--- src/main/java/baseball/models/Computer.java
+++ src/main/java/baseball/models/Computer.java
@@ -8,16 +8,21 @@ public class Computer implements User {
     private List<Integer> ball = new ArrayList<>();
 
     public Computer() {
+        createBall();
+    }
+
+    @Override
+    public List<Integer> getBall() {
+        return ball;
+    }
+
+    @Override
+    public void createBall() {
         Random random = new Random();
         while(ball.size() < 3) {
             int randomNumber = random.nextInt(9) + 1;
             if(!ball.contains(randomNumber))
                 ball.add(randomNumber);
         }
-    }
-
-    @Override
-    public List<Integer> getBall() {
-        return ball;
     }
 }
--- src/main/java/baseball/models/Player.java
+++ src/main/java/baseball/models/Player.java
@@ -4,16 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player implements User {
+    private String[] playerNumber;
     private List<Integer> ball = new ArrayList<>();
 
     public Player(String[] playerNumber) {
-        for(String number: playerNumber) {
-            ball.add(Integer.parseInt(number));
-        }
+        this.playerNumber = playerNumber;
+        createBall();
     }
 
     @Override
     public List<Integer> getBall() {
         return ball;
+    }
+
+    @Override
+    public void createBall() {
+        for(String number: playerNumber) {
+            ball.add(Integer.parseInt(number));
+        }
     }
 }
