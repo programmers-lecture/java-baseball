c0c5db5dec841f1d227ba8a001056e5cc87b9ea0
gobeomjun <alap_u@naver.com>
2022-09-21 15:29:34 +0900

    feat - 랜덤숫자 생성

---
+++ src/main/java/baseball/util/RandomNumberImpl.java
@@ -0,0 +1,19 @@
+package baseball.util;
+
+import baseball.domain.Ball;
+import baseball.domain.Balls;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RandomNumberImpl implements RandomNumber{
+
+
+    @Override
+    public List<Integer> createRandomNumbers() {
+       return Stream.generate(()->(int)Math.random()* Ball.MAX_BALL_NUMBER+Ball.MIN_BALL_NUMBER)
+           .distinct()
+           .limit(Balls.BALL_SIZE)
+           .collect(Collectors.toList());
+    }
+}
