db5ea861e0be0e1235116bd80605af2c10a2fec8
JEONG-JIWHAN <jjw981210@gmail.com>
2022-09-22 17:21:29 +0900

    GameController 구현

---
+++ src/main/java/Controller/GameController.java
@@ -0,0 +1,57 @@
+package Controller;
+
+
+import java.util.List;
+
+import Domain.Hint;
+import Domain.HintService;
+import View.InputView;
+import View.OutputView;
+import util.RandomNumber;
+
+public class GameController {
+  InputView inputView;
+  RandomNumber randomNumber;
+  HintService hintService;
+  OutputView outputView;
+
+  public GameController(InputView inputView, RandomNumber randomNumber, HintService hintService, OutputView outputView) {
+    this.inputView = inputView;
+    this.outputView = outputView;
+    this.randomNumber = randomNumber;
+    this.hintService = hintService;
+  }
+
+  public void run(){
+    playGame();
+    restartGame();
+  }
+
+  private void playGame(){
+
+    while(true){
+      List<Integer> playerNumbers = inputView.inputDisplay();
+      List<Integer> computerNumbers = randomNumber.createRandomNumbers();
+      Hint hint = hintService.countStrikeAndBall(playerNumbers, computerNumbers);
+      if(hint.getStrike()==3){
+        outputView.closeRightDisplay();
+        break;
+      }
+      outputView.strikeAndBallDisplay(hint.getStrike(), hint.getBall());
+    }
+  }
+
+  private void restartGame(){
+    while(checkRestart()){
+      playGame();
+    }
+  }
+
+  private boolean checkRestart(){
+    int replayValue = inputView.mainDisplay();
+    if(replayValue==1){
+      return true;
+    }
+    return false;
+  }
+}
