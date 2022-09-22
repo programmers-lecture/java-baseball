6bf0ea2bf7efaa3aad676cb089459f9bcac02b34
JEONG-JIWHAN <jjw981210@gmail.com>
2022-09-22 10:40:29 +0900

    랜덤넘버 생성

---
+++ src/main/java/util/RandomNumber.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class RandomNumber {
+  private static final int MAX_NUM = 9;
+  public int createRandomNumber(){
+    Random random = new Random();
+    int number = random.nextInt(MAX_NUM)+1;
+    return number;
+  }
+
+  public boolean validateDuplicateNumber(int number, List<Integer> randomNumbers){
+    if(randomNumbers.contains(number)){
+      return false;
+    }
+    return true;
+  }
+
+  public void addRandomNumber(int number, List<Integer> randomNumbers){
+    if(validateDuplicateNumber(number,randomNumbers)){
+      randomNumbers.add(number);
+    }
+  }
+
+  public List<Integer> createRandomNumbers(){
+    List<Integer> randomNumbers = new ArrayList<>();
+    while(randomNumbers.size()<3){
+      int number = createRandomNumber();
+      addRandomNumber(number,randomNumbers);
+    }
+    return randomNumbers;
+  }
+
+  public static void main(String[] args) {
+    RandomNumber randomNumber = new RandomNumber();
+    System.out.println(randomNumber.createRandomNumbers());
+  }
+}
