1dda7b6b228c6ccd06421be9f37f425de3293ca7
K-jun98 <alap_u@naver.com>
2022-09-24 01:16:31 +0900

    Refactor - 사용하지 않는 변수 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,6 @@ package baseball;
 import baseball.contoller.Game;
 
 public class Application {
-
-    private static boolean playGame = true;
-
     private static Game game;
     public static void main(String[] args) {
         while (game.getIsReplay()) {
