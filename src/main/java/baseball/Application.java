3c298b8b7a6ff3311e88b059c78de428fe03cc4d
K-jun98 <alap_u@naver.com>
2022-09-24 01:19:53 +0900

    Refactor - Static 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,14 +3,12 @@ package baseball;
 import baseball.contoller.Game;
 
 public class Application {
-    private static Game game;
     public static void main(String[] args) {
+        Game game = null;
         while (game.getIsReplay()) {
             game = new Game();
             game.run();
             game.userIsReplay();
         }
     }
-
-
 }
