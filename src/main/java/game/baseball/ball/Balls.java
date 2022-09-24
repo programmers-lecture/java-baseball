5e4c46090ff585eca3e200468a8c22f44b927180
hyena0608 <goatsfish@gmail.com>
2022-09-24 23:55:51 +0900

    feat : 올바르지 않은 공 개수 예외처리 기능 추가

--- src/main/java/game/baseball/ball/Balls.java
+++ src/main/java/game/baseball/ball/Balls.java
@@ -1,14 +1,26 @@
 package game.baseball.ball;
 
+import game.baseball.util.setting.GameSetting;
+
 import java.util.Collections;
 import java.util.List;
+
+import static game.baseball.message.ExceptionMessage.*;
+import static game.baseball.util.setting.GameSetting.*;
 
 public class Balls {
 
     private final List<Integer> balls;
 
     public Balls(List<Integer> balls) {
+        checkBallSizeOrThrow(balls);
         this.balls = balls;
+    }
+
+    private void checkBallSizeOrThrow(List<Integer> balls) {
+        if (balls.size() != GAME_SETTING.getBallSize()) {
+            throw new IllegalArgumentException(INCORRECT_BALL_SIZE.getErrorMessage());
+        }
     }
 
     public List<Integer> getBalls() {
--- src/main/java/game/baseball/message/ExceptionMessage.java
+++ src/main/java/game/baseball/message/ExceptionMessage.java
@@ -2,7 +2,8 @@ package game.baseball.message;
 
 public enum ExceptionMessage {
     RANDOM_NUMBER_GENERATOR_ERROR("올바르지 않은 랜덤 오류 입니다."),
-    INPUT_FORMAT_ERROR("올바르지 않은 포맷입니다.");
+    INPUT_FORMAT_ERROR("올바르지 않은 포맷입니다."),
+    INCORRECT_BALL_SIZE("입력된 공의 개수가 올바르지 않습니다.");
 
     private final String errorMessage;
 
