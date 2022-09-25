8364383e9f72a533710d1f369f88120be08ae46d
hyena0608 <goatsfish@gmail.com>
2022-09-25 13:59:39 +0900

    feat : 정규표현식을 이용한 사용자 입력 검증 및 재입력 요청 기능 구현

--- src/main/java/game/baseball/view/input/InputHandler.java
+++ src/main/java/game/baseball/view/input/InputHandler.java
@@ -1,31 +1,37 @@
 package game.baseball.view.input;
 
+import game.baseball.view.output.OutputView;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static game.baseball.message.ExceptionMessage.INPUT_FORMAT_ERROR;
+import static game.baseball.util.setting.GameSetting.GAME_SETTING;
 
 public class InputHandler {
 
     private final static InputView inputView = new InputView();
 
-    private InputHandler() {}
+    private InputHandler() {
+    }
 
     public static String readLine() {
         String input = "";
         input = inputView.read();
 
-        if (checkInputEmpty(input)) {
-            input = inputView.read().trim();
+        if (!checkDigit(input) || checkInputEmpty(input)) {
+            OutputView.printMessage(INPUT_FORMAT_ERROR.getErrorMessage());
+            input = InputHandler.readLine();
         }
 
         return input;
     }
 
-    public static String[] splitEach(String input) {
-        return Optional.of(input.split(""))
+    public static String[] splitEach(String inputBall) {
+        return Optional.of(inputBall.split(""))
                 .orElseThrow(() -> new IllegalArgumentException(INPUT_FORMAT_ERROR.getErrorMessage()));
     }
 
@@ -38,5 +44,12 @@ public class InputHandler {
 
     private static boolean checkInputEmpty(String input) {
         return input.length() == 0;
+    }
+
+    private static boolean checkDigit(String inputBall) {
+        return Pattern
+                .compile("^[1-9]{" + GAME_SETTING.getBallSize() + "}$")
+                .matcher(inputBall)
+                .find();
     }
 }
