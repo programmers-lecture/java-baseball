a65dcddbdf8d1849a4ec04ac607acbb09cb480ba
K-jun98 <alap_u@naver.com>
2022-09-26 18:37:36 +0900

    Refactor - 접근제어자 제거

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -5,7 +5,7 @@ public enum Score {
     STRIKE("스트라이크"), BALL("볼"), NOTHING("낫싱");
     private final String value;
 
-    private Score(String value) {
+    Score(String value) {
         this.value = value;
     }
     public String getValue() {
