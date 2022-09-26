eb8f46923f5169d464f26f4c15d03597b4a5cd8e
preferkim <shineecard5@gmail.com>
2022-09-27 07:06:55 +0900

    feat: 게임 시작 시 입력값 검증

--- src/main/java/baseballgame/view/InputView.java
+++ src/main/java/baseballgame/view/InputView.java
@@ -3,31 +3,33 @@ package baseballgame.view;
 import baseballgame.model.ConfirmType;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
     private static final String GUIDE_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String NOT_DIGIT_EXCEPTION_MESSAGE = "오류: 입력한 값이 숫자가 아닙니다.";
+    private static final String NOT_VALID_SIZE_EXCEPTION_MESSAGE = "오류: 3개의 숫자만 입력야합니다.";
     private static final int CONFIRM = 1;
     private static final int DECLINE = 2;
+    private static final int VALID_SIZE = 3;
 
     private final Scanner scanner = new Scanner(System.in);
 
     public ArrayList<Integer> readNumber() {
         System.out.print(GUIDE_MESSAGE);
-        String[] inputNumber = scanner.nextLine().split("");
+        String inputNumber = scanner.nextLine();
+        checkValidInput(inputNumber);
 
+        String[] numberArray = inputNumber.split("");
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (String number : inputNumber) {
+        for (String number : numberArray) {
             numbers.add(Integer.valueOf(number));
         }
-
         return numbers;
     }
 
     public ConfirmType readConfirmType() {
         int confirmType = scanner.nextInt();
-
         if (confirmType == CONFIRM) {
             return ConfirmType.CONFIRM;
         }
@@ -36,5 +38,27 @@ public class InputView {
 
     public void clearInput() {
         scanner.nextLine();
+    }
+
+    void checkValidInput(String inputNumber) {
+        if (!isDigit(inputNumber)) {
+            throw new IllegalStateException(NOT_DIGIT_EXCEPTION_MESSAGE);
+        }
+        if (!isValidSize(inputNumber)) {
+            throw new IllegalStateException(NOT_VALID_SIZE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isDigit(String inputNumber) {
+        try {
+            Integer.parseInt(inputNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isValidSize(String inputNumber) {
+        return inputNumber.length() == VALID_SIZE;
     }
 }
