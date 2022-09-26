819de402a0d58d9019f4d8e142d4ba9465460ac4
joona95 <joona95@naver.com>
2022-09-26 20:40:31 +0900

    refactor: static 동시성 문제 해결

--- src/main/java/baseball/views/InputView.java
+++ src/main/java/baseball/views/InputView.java
@@ -5,12 +5,11 @@ import baseball.models.Baseball;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner = new Scanner(System.in);
-
     private InputView() {}
 
     public static String[] inputPlayerNumber() {
         System.out.println("숫자를 입력해주세요 : ");
+        Scanner scanner = new Scanner(System.in);
         String[] playerNumber = scanner.next().split("");
         if(playerNumber.length > Baseball.BALL_SIZE)
             throw new IllegalArgumentException();
@@ -19,6 +18,7 @@ public class InputView {
 
     public static boolean inputRerunOrNot() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        Scanner scanner = new Scanner(System.in);
         return scanner.next().equals("1");
     }
 }
