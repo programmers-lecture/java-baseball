8f8669797a5af2a7c2948e77fa59e35226f58c14
joona95 <joona95@naver.com>
2022-09-27 08:21:18 +0900

    refactor: 변수 이름 변경 및 매직넘버 상수화

--- src/main/java/baseball/utils/RandomUtil.java
+++ src/main/java/baseball/utils/RandomUtil.java
@@ -3,12 +3,13 @@ package baseball.utils;
 import java.util.Random;
 
 public class RandomUtil {
-    private static final int NUMBER_MAX = 9;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
     private static final Random random = new Random();
 
     private RandomUtil() {}
 
     public static int createRandomNumber() {
-        return random.nextInt(NUMBER_MAX) + 1;
+        return random.nextInt(MAX_NUMBER) + MIN_NUMBER;
     }
 }
