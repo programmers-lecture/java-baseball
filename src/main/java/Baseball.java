9917c39a19391204dbde6a7fcfd04cca72e4265b
astrocat9311 <astrocat9311@gmail.com>
2022-11-16 23:16:10 +0900

    - out 처리까지 구현됨

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 
 public class Baseball {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         //준비 :  3개의 중복없는 난수가 담긴 list를 생성한다.
         Set<Integer> set = new HashSet<Integer>();
@@ -23,9 +24,11 @@ public class Baseball {
         //시작 : 사용자로부터 3개의 숫자를 입력받는다.
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < randomList.size(); i++) {
-            System.out.print(i + 1 + "번 째 숫자를 입력하세요 : ");
+        for(int i = 0; i < randomList.size(); i++) {
+            System.out.print(i + 1 + "번 째 숫자를 입력하세요");
             int num = sc.nextInt();
+
+
             inputList[i] = num;
         }
 
@@ -34,22 +37,37 @@ public class Baseball {
         sc.close();
 
         int strike = 0;
-        int ball = 0;
+        int ball   = 0;
 
-        for (int i = 0; i < randomList.size(); i++) {
-            for (int j = 0; j < inputList.length; j++) {
-                if (randomList.get(i) == inputList[j]) {
-                    if (i == j) {
-                        System.out.println("strike");
-                        strike++;
-                    } else {
-                        System.out.println("ball");
-                        ball++;
+        //만약 자동 생성된 난수와 사용자의 입력값을 set에 담았을 때 길이가 6이라면 하나도 맞추지 못했기 때문에 out 처리한다.
+        HashSet<Integer> isThereDuplicates = new HashSet<Integer>();
+
+        isThereDuplicates.addAll(Arrays.asList(inputList));
+        isThereDuplicates.addAll(randomList);
+
+        System.out.println("중복된 숫자 : " + isThereDuplicates);
+
+        if(isThereDuplicates.size() == 6) {
+            System.out.println("Out! 하나도 맞추지 못하셨네요~!");
+        } else {
+            for (int i = 0; i < randomList.size(); i++) {
+                for (int j = 0; j < inputList.length; j++) {
+                    if (randomList.get(i) == inputList[j]) {
+                        if (i == j) {
+                            System.out.println("strike");
+                            strike++;
+                        } else {
+                            System.out.println("ball");
+                            ball++;
+                        }
                     }
                 }
-            }
 
+            }
+            System.out.println(strike + ": strike(s) " + ball + " : ball(s)");
         }
+
+
 
     }
 }
