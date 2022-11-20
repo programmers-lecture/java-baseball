b202dcff88389f85cfc75c72f49c908d3870b83f
astrocat9311 <astrocat9311@gmail.com>
2022-11-20 22:14:36 +0900

    - 함수 단위로 기능을 분리 - 게임 종료 후 사용자의 선택에 따라 게임을 계속 또는 중단할 수 있도록 처리

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,11 +1,25 @@
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.*;
 
 
 public class Baseball {
     public static void main(String[] args) {
 
+        ArrayList<Integer> randomList = createRandomNumList();
+
+        Integer[] inputList = saveUserInputs(randomList.size());
+
+        int isContinue = printResult(randomList, inputList);
+
+        if (isContinue == 1) {
+            main(null);
+        } else {
+            System.out.println("게임을 종료합니다.");
+        }
+    }
+
+
+    //중복이 없는 3개의 랜덤 숫자를 담은 배열을 반환하는 함수.
+    public static ArrayList createRandomNumList() {
         //준비 :  3개의 중복없는 난수가 담긴 list를 생성한다.
         Set<Integer> set = new HashSet<Integer>();
         Random rand = new Random();
@@ -15,59 +29,64 @@ public class Baseball {
             set.add(num);
         }
 
-
         ArrayList<Integer> randomList = new ArrayList<Integer>(set);
-        Integer[] inputList = new Integer[3];
-        //ArrayList<Integer> inputList    = new ArrayList<Integer>(3); 이렇게 사용하면 아래에서 out of bound 에러가 발생한다...이유를 알고싶습니다.
-        System.out.println("생성된 숫자 : " + randomList);
+        //System.out.println("생성된 숫자 : " + randomList);
 
+        return randomList;
+    }
+
+    //사용자로부터 3개의 숫자를 입력받아 배열에 담아 반환하는 함수.
+    public static Integer[] saveUserInputs(int randomListSize) {
         //시작 : 사용자로부터 3개의 숫자를 입력받는다.
+        Integer[] inputList = new Integer[randomListSize];
+
         Scanner sc = new Scanner(System.in);
-
-        for(int i = 0; i < randomList.size(); i++) {
-            System.out.print(i + 1 + "번 째 숫자를 입력하세요");
+        for(int i = 0; i < randomListSize; i++) {
+            System.out.print(i + 1 + "번 째 숫자를 입력하세요 ");
             int num = sc.nextInt();
-
 
             inputList[i] = num;
         }
 
-        System.out.println("입력한 숫자 : " + Arrays.asList(inputList));
+        //System.out.println("입력한 숫자 : " + Arrays.asList(inputList));
 
-        sc.close();
+        return inputList;
+    }
+
+    public static int printResult(ArrayList<Integer> randomList, Integer[] inputList) {
+        //만약 자동 생성된 난수와 사용자의 입력값을 set에 담았을 때 길이가 6이라면 하나도 맞추지 못했기 때문에 out 처리한다.
+        HashSet<Integer> noDuplicates = new HashSet<Integer>();
 
         int strike = 0;
         int ball   = 0;
 
-        //만약 자동 생성된 난수와 사용자의 입력값을 set에 담았을 때 길이가 6이라면 하나도 맞추지 못했기 때문에 out 처리한다.
-        HashSet<Integer> isThereDuplicates = new HashSet<Integer>();
+        noDuplicates.addAll(Arrays.asList(inputList));
+        noDuplicates.addAll(randomList);
 
-        isThereDuplicates.addAll(Arrays.asList(inputList));
-        isThereDuplicates.addAll(randomList);
+        //System.out.println("중복된 숫자 : " + noDuplicates);
 
-        System.out.println("중복된 숫자 : " + isThereDuplicates);
-
-        if(isThereDuplicates.size() == 6) {
+        if(noDuplicates.size() == 6) {
             System.out.println("Out! 하나도 맞추지 못하셨네요~!");
         } else {
             for (int i = 0; i < randomList.size(); i++) {
                 for (int j = 0; j < inputList.length; j++) {
                     if (randomList.get(i) == inputList[j]) {
                         if (i == j) {
-                            System.out.println("strike");
+                            //System.out.println("strike");
                             strike++;
                         } else {
-                            System.out.println("ball");
+                            //System.out.println("ball");
                             ball++;
                         }
                     }
                 }
-
             }
             System.out.println(strike + ": strike(s) " + ball + " : ball(s)");
         }
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("게임을 다시 시작하시겠습니까? 1.예 2.아니오");
 
-
+        return sc.nextInt();
     }
 }
