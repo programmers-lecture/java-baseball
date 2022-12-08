0dc56c53999199a4259dc7ad0d2376aaaba34e1a
minkyeng <jomk0609@inzent.com>
2022-12-08 17:12:55 +0900

    feat : 3자리의 수를 입력

--- src/main/java/com/programmers/baseball/Baseball.java
+++ src/main/java/com/programmers/baseball/Baseball.java
@@ -1,7 +1,9 @@
 package com.programmers.baseball;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Baseball {
     public static void main(String[] args) {
@@ -9,5 +11,10 @@ public class Baseball {
 
         Random random = new Random();
         int[] computerNumber = IntStream.generate(() -> random.nextInt(9) + 1).distinct().limit(NUMBER_SIZE).toArray();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+
+        int[] guessNumber = Stream.of(String.valueOf(scanner.nextInt()).split("")).mapToInt(Integer::parseInt).toArray();
     }
 }
