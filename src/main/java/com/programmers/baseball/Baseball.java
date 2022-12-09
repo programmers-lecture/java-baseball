e05b1fc0095bc69d3ad384d14c493a3efe30903d
minkyeng <jomk0609@inzent.com>
2022-12-09 14:55:41 +0900

    feat : 게임이 종료 된 후, 게임을 다시 시작하거나 완전히 종료

--- src/main/java/com/programmers/baseball/Baseball.java
+++ src/main/java/com/programmers/baseball/Baseball.java
@@ -9,45 +9,51 @@ import java.util.stream.Stream;
 public class Baseball {
     public static void main(String[] args) {
         final int NUMBER_SIZE = 3;
-        int strike = 0;
-        int ball;
+        int gameOnGoing = 1;
 
-        Random random = new Random();
-        int[] computerNumber = IntStream.generate(() -> random.nextInt(9) + 1).distinct().limit(NUMBER_SIZE).toArray();
+        while (gameOnGoing==1) {
+            Random random = new Random();
+            int[] computerNumber = IntStream.generate(() -> random.nextInt(9) + 1).distinct().limit(NUMBER_SIZE).toArray();
 
-        while (strike!=3) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("숫자를 입력해주세요 : ");
+            int strike = 0;
+            int ball = 0;
 
-            int[] guessNumber = Stream.of(String.valueOf(scanner.nextInt()).split("")).mapToInt(Integer::parseInt).toArray();
-            if (guessNumber.length != NUMBER_SIZE) throw new IllegalArgumentException(NUMBER_SIZE + "자리의 수를 입력하세요.");
+            while (strike != 3) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("숫자를 입력해주세요 : ");
 
-            boolean duplicated = Arrays.stream(guessNumber).distinct().count() != guessNumber.length;
-            if (duplicated) throw new IllegalArgumentException("각 자리의 값을 중복되지 않게 입력해주세요.");
+                int[] guessNumber = Stream.of(String.valueOf(scanner.nextInt()).split("")).mapToInt(Integer::parseInt).toArray();
+                if (guessNumber.length != NUMBER_SIZE) throw new IllegalArgumentException(NUMBER_SIZE + "자리의 수를 입력하세요.");
 
-            strike = 0;
-            ball = 0;
+                boolean duplicated = Arrays.stream(guessNumber).distinct().count() != guessNumber.length;
+                if (duplicated) throw new IllegalArgumentException("각 자리의 값을 중복되지 않게 입력해주세요.");
 
-            for (int i = 0; i < NUMBER_SIZE; i++) {
-                for (int j = 0; j < NUMBER_SIZE; j++) {
-                    if (i == j) {
-                        if (computerNumber[i] == guessNumber[j]) {
-                            strike++;
-                        }
-                    } else {
-                        if (computerNumber[i] == guessNumber[j]) {
-                            ball++;
+                strike = 0;
+
+                for (int i = 0; i < NUMBER_SIZE; i++) {
+                    for (int j = 0; j < NUMBER_SIZE; j++) {
+                        if (i == j) {
+                            if (computerNumber[i] == guessNumber[j]) {
+                                strike++;
+                            }
+                        } else {
+                            if (computerNumber[i] == guessNumber[j]) {
+                                ball++;
+                            }
                         }
                     }
                 }
-            }
 
-            if (ball != 0) System.out.print(ball + "볼 ");
-            if (strike != 0) System.out.println(strike + "스트라이크 ");
-            if (ball == 0 && strike == 0) System.out.println("낫싱");
+                if (ball != 0) System.out.print(ball + "볼 ");
+                if (strike != 0) System.out.print(strike + "스트라이크 ");
+                if (ball == 0 && strike == 0) System.out.print("낫싱");
+                System.out.println();
 
-            if (strike == NUMBER_SIZE) {
-                System.out.println(NUMBER_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                if (strike == NUMBER_SIZE) {
+                    System.out.println(NUMBER_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    gameOnGoing = scanner.nextInt();
+                }
             }
         }
     }
