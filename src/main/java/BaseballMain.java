dc72e2d9eb874dc823ec572e702f9a5bdd97b768
s2s22 <sycho3713@hanmail.net>
2022-12-12 21:12:27 +0900

    pull request 수정 - 변수명 명확 - IllegalArgumentException 예외발생

--- src/main/java/BaseballMain.java
+++ src/main/java/BaseballMain.java
@@ -5,32 +5,46 @@ import java.util.stream.Stream;
 
 public class BaseballMain {
     public static void main(String[] args) {
+
+        // 1-9 임의의 수 3개 생성
         IntStream ints = new Random().ints(0,10);
-        List<String> list = ints.distinct().limit(3).boxed().map(Object::toString).collect(Collectors.toList());
-        System.out.println(list);
-            Scanner scan = new Scanner(System.in);
-        List list2 = new LinkedList();
-        int strike = 0;
-        int ball = 0;
+        List<String> randomNum = ints.distinct().limit(3).boxed().map(Object::toString).collect(Collectors.toList());//랜덤 숫자
+
+        Scanner scan = new Scanner(System.in);
+
+        List inputNum = new LinkedList();// 입력받을 변수
+
+        int strike = 0;// 스트라이크 개수 변수
+        int ball = 0; // 볼 개수 변수
+
         while (true) {
-            list2 = new LinkedList();
+            inputNum = new LinkedList();
             strike = 0;
             ball= 0;
             System.out.print("숫자를 입력해주세요 : ");
             String str = scan.nextLine();
-            list2 = Arrays.stream(str.split("")).collect(Collectors.toList());
-            //스트라이크
-            for(int i =0;i<list.size();i++) {
-                if(list.get(i).equals(list2.get(i))) {
+            inputNum = Arrays.stream(str.split("")).collect(Collectors.toList());
+
+            //입력 검증
+            if(inputNum.stream().filter(num -> Character.isDigit(num.toString().charAt(0))).count() < 3 || inputNum.size() > 3) { //입력값이 숫자가 아닐때 || 입력이 4글자 이상일때
+                throw new IllegalArgumentException("띄어쓰기 없이 숫자 3개를 입력해주세요");
+            };
+
+
+
+            //스트라이크 개수 세기 기능
+            for(int i =0;i<randomNum.size();i++) {
+                if(randomNum.get(i).equals(inputNum.get(i))) {
                     strike++;
                 }
             }
 
-            //볼
-            list2.retainAll(list);
-            ball = list2.size() - strike;
+            //볼 개수 세기 기능
+            inputNum.retainAll(randomNum);
+            ball = inputNum.size() - strike;
 
-            if(strike ==0 && ball ==0 ) System.out.println("낫싱");
+            // 결과 출력
+            if(strike ==0 && ball == 0 ) System.out.println("낫싱");
             else System.out.println(ball + "볼 " + strike + "스트라이크");
 
             if(strike == 3) {
@@ -38,6 +52,9 @@ public class BaseballMain {
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 String next = scan.nextLine();
 
+                if(!"1".equals(next) || !"2".equals(next)) {
+                    throw new IllegalArgumentException("1 또는 2를 입력해주세요");
+                }
                 if(next.equals("1")) continue;
                 if(next.equals("2")) break;
 
@@ -45,12 +62,7 @@ public class BaseballMain {
         }
 
 
-
-
-
-
-
-
-
     }
+
+
 }
