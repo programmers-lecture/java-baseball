fe9179e59493c47606d64b65672138bd2486f8be
namgonkim <32921225+namgonkim@users.noreply.github.com>
2022-12-16 13:06:26 +0900

    [refactor] message 인스턴스 생성 금지

--- src/main/java/application/constant/Message.java
+++ src/main/java/application/constant/Message.java
@@ -8,6 +8,8 @@ public class Message {
     private static final String THREE_STRIKE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private Message() { }
+
     public static String getInputNumberToPlay() {
         return INPUT_NUMBER_TO_PLAY;
     }
