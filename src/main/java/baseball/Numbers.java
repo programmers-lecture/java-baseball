57ab8651541812f069846a0e2b839bb4724bd071
cdj2073 <79151181+cdj2073@users.noreply.github.com>
2022-12-26 11:23:13 +0900

    feat: getNumberString

--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -1,18 +1,24 @@
 package baseball;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Numbers {
     private int size;
-    private ArrayList<Integer> numberList;
+    private String numberString;
+    private HashSet<Integer> numberSet;
     private RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
     Numbers(int size) {
         this.size = size;
-        HashSet<Integer> numberSet = new HashSet<Integer>();
+        numberSet = new HashSet<Integer>();
         while (numberSet.size() < size) {
             numberSet.add(randomNumberGenerator.generate());
         }
-        numberList = new ArrayList<>(numberSet);
+        numberString = numberSet.stream().map(Object::toString).collect(Collectors.joining());
+    }
+
+    public String getNumberString() {
+        return numberString;
     }
 }
